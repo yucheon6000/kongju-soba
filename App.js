c54afd2e1ce626b7ef423c50a31d5b160d60fbfa
yucheon6000 <yucheon6000@gmail.com>
2022-02-04 23:29:03 +0900

    공주소바 첫 커밋

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import MainScreen from "./assets/js/screen/MainScreen";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <MainScreen />
+        );
+    }
+}
+
+export default App;
---
+++ assets/js/screen/MainScreen.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+export type Props = {};
+
+class MainScreen extends React.Component<Props> {
+    constructor(props: Props) {
+        super(props);
+    }
+
+    componentDidMount() {
+
+    }
+
+    render() {
+        return (
+            <View>
+                <Text></Text>
+            </View>
+        );
+    }
+}
+
+export default MainScreen;
